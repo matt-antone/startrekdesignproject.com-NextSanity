e38905a5df8d3ee023189d95967e9c32a9795989
Matthew Antone <matt.antone@gmail.com>
2024-04-09 13:40:22 -0700

    really random now

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import { client } from "@/sanity/lib/client";
 import PortableText from "../components/PortableText";
+import RandomPosts from "../components/RandomPosts";
 
 export default async function Home() {
-  const indexSeed1 = Math.floor(Math.random() * 500);
-  const indexSeed2 = Math.floor(Math.random() * 1000);
   const query = `*[_type == "home"]{
     ...,
     "heroImage": {
@@ -13,18 +12,7 @@ export default async function Home() {
       caption,
       "width": hero.heroImage.asset->metadata.dimensions.width,
       "height": hero.heroImage.asset->metadata.dimensions.height
-    },
-    "randos": *[_type == "post"]{
-      title,
-      "slug": slug.current,
-      "featuredImage": {
-        "src": featuredImage.asset->url,
-        alt,
-        caption,
-        "width": featuredImage.asset->metadata.dimensions.width,
-        "height": featuredImage.asset->metadata.dimensions.height
-      }
-    }[${indexSeed1}...${indexSeed1+4}],
+    }
   }[0]`;
   const post = await client.fetch(query);
   console.log("post", post.randos);
@@ -44,34 +32,8 @@ export default async function Home() {
           {post?.hero?.body && <PortableText value={post.hero.body} />}
         </div>
       )}
-          <h2 className="text-center my-12 text-2xl font-bold">Random Posts</h2>
-          <div className="grid grid-cols-2 lg:grid-cols-4 gap-12 items-center">
-            {post.randos ? post.randos.map((symbol: any) => {
-              return (
-                <div key={symbol.slug} className="mb-12">
-                  {symbol.featuredImage && (
-                    <img
-                      src={symbol.featuredImage.src}
-                      alt={symbol.title}
-                      width={symbol.featuredImage.width / 1.5}
-                      height={symbol.featuredImage.height / 1.5}
-                      className="mx-auto block"
-                    />
-                  )}
-                  <h2 className="text-center">
-                    <a href={`/symbols/${symbol.slug}`}>{symbol.title}</a>
-                  </h2>
-                </div>
-              );
-            }) : (
-              <>
-                <div>loading</div>
-                <div>loading</div>
-                <div>loading</div>
-                <div>loading</div>
-              </>
-            )}
-          </div>
+        <RandomPosts />
+
 
       {post.body && <PortableText value={post.body} />}
     </main>
---
+++ src/app/components/RandomPosts.tsx
@@ -0,0 +1,77 @@
+"use client";
+import * as React from "react";
+import { client } from "@/sanity/lib/client";
+import { get } from "http";
+
+interface IRandomPostsProps {
+  post: any;
+}
+
+const RandomPosts: React.FunctionComponent<IRandomPostsProps> = ({ post }) => {
+  const indexSeed1 = Math.floor(Math.random() * 500);
+  const [posts, setPosts] = React.useState([]);
+  React.useEffect(() => {
+    const query = `*[_type == "post"]{
+      title,
+      "slug": slug.current,
+      "featuredImage": {
+        "src": featuredImage.asset->url,
+        alt,
+        caption,
+        "width": featuredImage.asset->metadata.dimensions.width,
+        "height": featuredImage.asset->metadata.dimensions.height
+      }
+    }[${indexSeed1}...${indexSeed1 + 4}]`;
+    console.log("query", query);
+    const getPosts = async () => {
+      const posts = await client.fetch(query);
+      setPosts(posts);
+    };
+    getPosts();
+  }, []);
+  console.log("posts", posts);
+  return (
+    <div>
+      <h2 className="text-center my-12 text-2xl font-bold">Random Posts</h2>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-12 items-center text-center">
+        {posts.length > 0 ? (
+          posts.map((symbol: any) => {
+            return (
+              <div key={symbol.slug} className="mb-12">
+                {symbol.featuredImage && (
+                  <img
+                    src={symbol.featuredImage.src}
+                    alt={symbol.title}
+                    width={symbol.featuredImage.width / 1.5}
+                    height={symbol.featuredImage.height / 1.5}
+                    className="mx-auto block"
+                  />
+                )}
+                <h2 className="text-center">
+                  <a href={`/symbols/${symbol.slug}`}>{symbol.title}</a>
+                </h2>
+              </div>
+            );
+          })
+        ) : (
+          <>
+            <div className="w-64 h-64 max-w-[100%] flex justify-center items-center text-center">
+              loading
+            </div>
+            <div className="w-64 h-64 max-w-[100%] flex justify-center items-center text-center">
+              loading
+            </div>
+            <div className="w-64 h-64 max-w-[100%] flex justify-center items-center text-center">
+              loading
+            </div>
+            <div className="w-64 h-64 max-w-[100%] flex justify-center items-center text-center">
+              loading
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RandomPosts;
