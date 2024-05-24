2d7a4556948f0f3f0c102b7d3a7fc7cd7a2b140a
Matthew Antone <matt.antone@gmail.com>
2024-05-24 10:41:19 -0700

    added update algolia route

---
+++ src/lib/queries/index.ts
@@ -0,0 +1,21 @@
+import { groq } from "next-sanity";
+export const SITEMAP_QUERY = groq`{
+  "test": *[_type == "post"]|order(date desc){
+    "objectID": _id,
+    image{...,asset->{
+      url,
+      metadata{dimensions}
+    }},
+    "slug": slug.current,
+    date,
+    title,
+    excerpt,
+    body,
+    "timePeriod": timePeriod->title,
+    "quadrant": quadrant->title,
+    "universes": universes[]->title,
+    "affiliations": affiliations[]->title,
+    "types": types[]->title,
+    "franchise": franchise->title,
+    references,
+}`;
