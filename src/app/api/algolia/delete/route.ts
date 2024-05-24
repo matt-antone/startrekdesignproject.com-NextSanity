d00baa1f7c64dac9bf46d7ffc75bcaad4e1c18d3
Matthew Antone <matt.antone@gmail.com>
2024-05-24 14:05:18 -0700

    more accurate debugging

--- src/app/api/algolia/delete/route.ts
+++ src/app/api/algolia/delete/route.ts
@@ -8,17 +8,19 @@ export async function POST(request: Request) {
   try {
     // POST THE LATEST POSTS
     const res = await request.json();
-    console.log("Deleting", res);
+    console.log("Deleting", res._id);
     // Add the post to Algolia
     const client = algoliasearch(
       process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || "",
       process.env.ALGOLIA_SEARCH_ADMIN_KEY || ""
     );
     const index = client.initIndex(res._type);
-    const algoliaResponse = await index.deleteObject(res._id);
-    console.log(algoliaResponse);
-    revalidatePath("/symbols/" + res.slug);
-    return new Response("Hello world!");
+    const algoliaResponse = await index.deleteObject(res._id).then((del:any) => {
+      console.log(del);
+      revalidatePath("/symbols/");
+      revalidatePath("/symbols/" + res.slug);
+      return new Response("Hello world!");
+    });;
   } catch (error) {
     console.error("Error updating Algolia", error);
     return new Response("Error updating Algolia");
