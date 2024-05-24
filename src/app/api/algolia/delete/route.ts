a9b1b75e16808d1ea57ba86d98c4b68b85a905b1
Matthew Antone <matt.antone@gmail.com>
2024-05-24 13:11:30 -0700

    added algolia delete

---
+++ src/app/api/algolia/delete/route.ts
@@ -0,0 +1,25 @@
+export const dynamic = "force-dynamic"; // defaults to auto
+import algoliasearch from "algoliasearch";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function POST(request: Request) {
+  try {
+    // POST THE LATEST POSTS
+    const res = await request.json();
+    const remove = [res._id];
+  
+
+    // Add the post to Algolia
+    const client = algoliasearch(
+      process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || "",
+      process.env.ALGOLIA_ADMIN_KEY || ""
+    );
+    const index = client.initIndex(res._type);
+    const algoliaResponse = await index.deleteObjects(remove);
+    return new Response("Hello world!");
+  } catch (error) {
+    console.error("Error updating Algolia", error);
+    return new Response("Error updating Algolia");
+  }
+}
