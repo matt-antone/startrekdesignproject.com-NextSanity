c7210909598e170aa3c53b6660bce236af469b03
Matthew Antone <matt.antone@gmail.com>
2024-06-06 10:14:02 -0700

    added search params as title

--- src/app/components/PageHeader.tsx
+++ src/app/components/PageHeader.tsx
@@ -1,14 +1,27 @@
+"use client";
 import * as React from 'react';
+import { useSearchParams } from 'next/navigation';
 
 interface IPageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FunctionComponent<IPageHeaderProps> = ({title}) => {
+const Header: React.FunctionComponent<IPageHeaderProps>  = ({title}) => {
+  const searchParams = useSearchParams();
+  const taxonomy = searchParams.get('tax') || '';
+  const term = searchParams.get('term') || '';
   return (
     <div>
-      <h1 className='text-3xl font-bold mb-8'>{title}</h1>
+      <h1 className='capitalize text-3xl font-bold mb-8'>{term ? `${taxonomy} - ${term}` : title}</h1>
     </div>
+  );  
+}
+
+const PageHeader: React.FunctionComponent<IPageHeaderProps> = ({title}) => {
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+    <Header title={title} />
+    </React.Suspense>
   );
 };
 
